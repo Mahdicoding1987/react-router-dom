338170e3dfe68685ea5d412d3a3b6bb7ac5808b8
Mahdicoding1987 <mahdilogistics@gmail.com>
2024-11-07 14:59:42 +0300

    Complete

---
+++ src/components/NewPokemon.jsx
@@ -0,0 +1,11 @@
+const NewPokemon = () => {
+  return (
+    <>
+      <h2>Add New Pokemon</h2>
+      <input placeholder="Type New Pokemon"></input>
+      <button>Add</button>
+    </>
+  );
+};
+
+export default NewPokemon;
